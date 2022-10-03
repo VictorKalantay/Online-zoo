1661c7c8c1f05c2c488962f46bb1202b2ebc927d
Victor Kalantay <v.kalantay@ukr.net>
2022-10-04 01:18:35 +0300

    feat:add interactivity for burger and donation page

--- online-zoo/assets/js/script.js
+++ online-zoo/assets/js/script.js
@@ -1,7 +1,11 @@
-let dotsCollection = document.querySelectorAll('.pay__dot')
+const dotsCollection = document.querySelectorAll('.pay__dot')
+const inputField = document.querySelector('.feed__input')
+inputField.value = 100
+let currentSum
 const changeDotClass = () => {
     dotsCollection.forEach(dot => {
         dot.addEventListener('click', event => {
+            currentSum = event.currentTarget.getAttribute('data-pay');
             dotsCollection.forEach(dot => {
                 if (dot.classList.contains('active')) {
                     dot.classList.remove('active')
@@ -9,8 +13,24 @@ const changeDotClass = () => {
             })
             let currentDot = event.currentTarget
             currentDot.classList.add('active')
+            inputField.value = currentSum
         })
     })
+    inputField.addEventListener('input', () => {
+       inputField.value = inputField.value.slice(0, 4)
+        dotsCollection.forEach(dot => {
+            dot.classList.remove('active')
+                     if(inputField.value === dot.getAttribute('data-pay')) {
+
+                         dot.classList.add('active')
+
+                     }
+        })
+    })
+
+
+
+
 }
 changeDotClass()
 
@@ -18,7 +38,7 @@ changeDotClass()
 //-------------------------------------------------
 // меню бургер
 
-
+const headerArea = document.querySelector('.header__area')
 const iconBurger = document.querySelector('.header__burger');
 const navMenu = document.querySelector('.header .nav__container');
 const logo = document.querySelector('.logo__container')
@@ -29,14 +49,32 @@ if (iconBurger) {
         navMenu.classList.toggle('active');
         logo.classList.toggle('active')
         currentLink.classList.toggle('nav__link--current')
+        headerArea.classList.toggle('active')
+
     });
+
+    headerArea.addEventListener('click', () => {
+        iconBurger.classList.toggle('active');
+        navMenu.classList.toggle('active');
+        logo.classList.toggle('active')
+        currentLink.classList.toggle('nav__link--current')
+        headerArea.classList.toggle('active')
+    })
+
+
 }
 
 if (iconBurger.classList.contains('active')) { //Если бургер развернут то уберем класс
     iconBurger.classList.remove('active');
     navMenu.classList.remove('active');
     logo.classList.remove('active')
+    currentLink.classList.remove('nav__link--current')
+    headerArea.classList.toggle('active')
+
+
 }
 
+
+
 //------------------------------
-//alert('Адаптив на брейкпоинтах: 1000px, 640px, 320px')
+alert('Адаптив на брейкпоинтах: 1000px, 640px, 320px')
